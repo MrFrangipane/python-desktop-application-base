f28c35a5709088b57d4c6bbd992560817d107fd1
MrFrangipane <contact@frangipane.me>
2025-01-06 15:55:35 +0100

    imp: remove dynamic version from pyproject.toml

--- desktopapplicationbase/dock_widget/dock_widget_api.py
+++ desktopapplicationbase/dock_widget/dock_widget_api.py
@@ -8,6 +8,7 @@ def register_dock_widget(widget: QWidget):
     main_window = main_window_api.get_instance()
 
     dock_widget = QDockWidget(widget.objectName())
+    dock_widget.setObjectName("Docked" + widget.objectName())
     dock_widget.setWidget(widget)
 
     main_window.addDockWidget(Qt.RightDockWidgetArea, dock_widget)
